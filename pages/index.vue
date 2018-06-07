c6452211663a510ecea6b3f0ee2195fa38d074db
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2018-06-07 19:30:18 +0100

    Installed nuxt, sass and pug

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  h1 Hello World!
+</template>
+
+<script>
+  export default {
+    head: {
+      title: 'Home',
+    },
+  }
+</script>
+
+<style lang="sass" scoped>
+  h1
+    color: blue
+</style>
