25b6d867c5b98084e8cae5d93e1bcdc64864e795
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2018-06-08 17:43:50 +0100

    Added centre store module

---
+++ store/centre.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+  offices: [
+    {
+      id: 0,
+      address: '15 Hyde Park Place',
+    },
+    {
+      id: 1,
+      address: '45 Marylebone Rd',
+    },
+    {
+      id: 2,
+      address: '3 London Bridge Street',
+    },
+  ],
+})
