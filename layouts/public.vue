fccd635f7dd9a7e85fa8335bd2a0fc465dc39fde
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2018-06-08 16:56:45 +0100

    Added public layout and login page

---
+++ layouts/public.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-app
+    v-content
+      v-container(fill-height)
+        v-layout(align-center justify-center)
+          v-flex(sm8)
+            .display-1.text-xs-center.mb-3.primary--text
+              v-icon.mr-2.mb-1(color='primary' large) business_center
+              | MyWorkspace
+            nuxt
+    the-footer
+</template>
+
+<script>
+  import TheFooter from '~/components/the-footer.vue'
+
+  export default {
+    components: { TheFooter },
+  }
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  v-card
+    v-card-text
+      .subheading.text-xs-center Log in to your customer portal
+      v-form
+        v-text-field(v-model='user.email' prepend-icon='email' label='Email' type='email')
+        v-text-field(v-model='user.password' prepend-icon='lock' label='Password' type='password')
+        v-checkbox(v-model='user.shouldStayLoggedIn' label='Stay logged in?' hide-details)
+        v-btn(to='/' color='primary' block type='submit') Sign in
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      user: {
+        email: 'chandler.bing@example.com',
+        password: 'sarc@stic_j0kes',
+        shouldStayLoggedIn: true,
+      },
+    }),
+    head: { title: 'Login' },
+    layout: 'public',
+  }
+</script>
