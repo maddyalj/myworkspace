55341e9607f16a4ce25f1fe377dc05819b3bfd7a
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2018-06-12 18:24:50 +0100

    Increased margins between login elements

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   v-card
     v-card-text
-      .subheading.text-xs-center Log in to your customer portal
+      .subheading.mb-3.text-xs-center Log in to your customer portal
       v-form(v-model='isValid' ref='form' @submit.prevent='submitForm')
-        v-alert(:value='error' type='error') Invalid email or password. Please try again.
+        v-alert.mb-3(:value='error' type='error') Invalid email or password. Please try again.
         v-text-field(v-model='account.email' :rules='[rules.required]' prepend-icon='email' label='Email' type='email')
         v-text-field(v-model='account.password' :rules='[rules.required]' prepend-icon='lock' label='Password' type='password')
         v-checkbox(v-model='account.shouldStayLoggedIn' label='Stay logged in?' hide-details)
